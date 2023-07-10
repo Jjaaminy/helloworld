7ae0cfd65726ad06302f8cec5071e5a921e1dd07
Jjaaminy <jaaminy18@gmail.com>
2023-07-10 11:07:06 +0200

    Initial commit

---
+++ src/test/java/ch/zli/m223/extended_hello_world/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zli.m223.extended_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
